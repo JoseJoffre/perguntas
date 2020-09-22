f9a34ae5a91e34ddcdea25de301d7f50813f16de
JoseJoffre <j.joffre.galvao@gmail.com>
2020-09-22 18:57:03 -0300

    Finalizando o formulário de perguntas e respostas.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require("body-parser")
 const connection = require("./database/database");
 const Pergunta = require("./database/Pergunta");
+const Resposta = require("./database/Resposta")
 
 //Database
 connection.authenticate().then(() => {
@@ -20,7 +21,15 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 // Rotas
 app.get("/", (req, res) => {
-    res.render("index");
+    Pergunta.findAll({
+        raw: true, order: [
+            ["id", "DESC"]
+        ]
+    }).then(perguntas => {
+        res.render("index", {
+            perguntas: perguntas
+        });
+    });
 });
 
 app.get("/perguntar", (req, res) => {
@@ -38,6 +47,42 @@ app.post("/salvarpergunta", (req, res) => {
         res.redirect("/")
     });
 });
+
+app.get("/pergunta/:id", (req, res) => {
+    let id = req.params.id;
+
+    Pergunta.findOne({
+        where: { id: id }
+    }).then(pergunta => {
+        if (pergunta != undefined) {
+            Resposta.findAll({
+                where: { perguntaId: pergunta.id }
+            }).then(respostas => {
+                res.render("pergunta", {
+                    pergunta: pergunta,
+                    respostas: respostas
+                });
+            });
+
+        } else {
+            res.redirect("/");
+        }
+    });
+
+});
+app.post("/responder", (req, res) => {
+    let corpo = req.body.corpo;
+    let perguntaId = req.body.pergunta;
+    Resposta.create({
+        corpo: corpo,
+        perguntaId: perguntaId,
+        order: [
+            ["id", "desc"]
+        ]
+    }).then(() => {
+        res.redirect("/pergunta/" + perguntaId);
+    })
+})
 
 app.listen(8080, () => {
     console.log("App tá rodando! \nNem errou dessa vez hein?");
